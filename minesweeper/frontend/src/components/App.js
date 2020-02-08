51ce7f10b68ee4b033df512879cb19005b50f756
Ayobami Ige <thorbaba01@gmail.com>
2020-02-08 10:10:15 -0600

    created basic front end view for app

---
+++ minesweeper/frontend/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from "react";
+import ReactDOM from "react-dom";
+
+class App extends Component {
+  render() {
+    return (
+     
+          <div className="container">
+            <h1>Mine</h1>
+          </div>
+       
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
